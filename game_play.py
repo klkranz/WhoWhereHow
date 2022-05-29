5d469b525d9046edccca4bf01a5edd117834edc2
klkranz <kkranz@protonmail.com>
2022-05-28 18:05:33 -0600

    Updated descriptions of all the rooms.

--- game_play.py
+++ game_play.py
@@ -27,7 +27,7 @@ class Location:
         self.connects_to = connects_to
 
     def __str__(self):
-        return f"{self.name} - {self.description} - connects to {self.connects_to}"
+        return f"The {self.name} is {self.description} - connects to {self.connects_to}"
 
 
 # Create a class for the Weapons that could be used by the suspects.
@@ -49,15 +49,43 @@ suspect3 = Suspect('Azure Hearth', 'Mrs.', 'blue', 'widowed daughter-in-law', "b
 suspect4 = Suspect('Lavender', 'Capt.', 'purple', 'friend from military days', 'victim knew his dark secret')
 suspect5 = Suspect('Golden', 'Dr.', 'yellow', 'employee, a personal physician', 'verbally abused by the victim')
 suspect6 = Suspect('Snow', 'Chef', 'white', 'employee, the cook for the household', 'victim spurned her affection')
-location1 = Location('Attic', 'fill in description.', False)
-location2 = Location('Master Bedroom', 'fill in description', True, 'Study')
-location3 = Location('Study', 'fill in description', True, 'Master Bedroom')
-location4 = Location('Conservatory', 'fill in description.', True, 'Laboratory')
-location5 = Location('Dining Room', 'fill in description', False)
-location6 = Location('Library', 'fill in description', False)
-location7 = Location('Kitchen', 'fill in description', False,)
-location8 = Location('Laboratory', 'fill in description', True, 'Conservatory')
-location9 = Location('Cellar', 'fill in description.', False)
+location1 = Location('Attic', 'a dusty storage space, piled with boxes, trunks and random items from generations '
+                              'of the Hearth family. Some odd things stand out such as a box of very rusted nails, '
+                              'a hollowed out grenade, a coil of electrical wire and a box of broken or cracked '
+                              'beakers and vials.', False)
+location2 = Location('Master Bedroom', 'lavishly appointed with a four-posted bed and an overstuffed chair next to '
+                                       'the fireplace. A nightstand containing a water pitcher, glass, and a tray '
+                                       'with an assortment of ointments, tinctures and bottles of pills. The wardrobe '
+                                       'has all the clothes pushed to one side.', True, 'Study')
+location3 = Location('Study', 'a comfortable room with wood paneling on the walls. The most imposing item in the room '
+                              'is a large wooden desk with ink stains, piles of papers, and many drawers including '
+                              'one with a lock. It looks like the lock has been tampered with at some point. There '
+                              'are two comfortable and well-used chairs placed in front of the fireplace with a '
+                              'small table conveniently placed for a tray of drinks.', True, 'Master Bedroom')
+location4 = Location('Conservatory', 'an indoor greenhouse filled with low planters and small potted plants. There '
+                                     'are some cushioned benches, chairs and small tables affording a peaceful spot '
+                                     'to enjoy the beauty of nature while still being comfortable. A door leads to a '
+                                     'small closet used like a shed for gardening tools', True, 'Laboratory')
+location5 = Location('Dining Room', 'an elegant dining space with a finely carved dining table and matching chairs. '
+                                    'A large sideboard displays the fine serving dishes, well-polished silverware, '
+                                    'ornate china plates and silver candlesticks. The crystal chandelier hangs above '
+                                    'the middle of the table.', False)
+location6 = Location('Library', 'lined with bookshelves filled with books. Some of the books are obviously covered '
+                                'in dust and others look frequently used. There are cushioned window-seats at the '
+                                'two large bay windows, and other comfortable chairs and a couch in two small '
+                                'conversation groups.', False)
+location7 = Location('Kitchen', 'a large but simple room decorated to be utilitarian not grand like the rest of the '
+                                'manor. Everything seems to have its place and there is no sign of any dust, crumb '
+                                'or stain except the old stains on the large wooden cutting board next to the block '
+                                'where all the knives are stored.', False,)
+location8 = Location('Laboratory', 'a cluttered and dirty room. The counters display signs of wear and stains from '
+                                   'past "experiments". Old beakers, tubes and other paraphernalia are scattered on '
+                                   'all available counters as if left in the middle of whatever project they had been '
+                                   'used in. The shelves above are covered with stoppered bottles, vials and boxes '
+                                   'mostly covered in dust as if left undisturbed for some time.', True, 'Conservatory')
+location9 = Location('Cellar', 'a small space at the bottom of the stairs from the Kitchen. The air is musty and the '
+                               'dirt floor is well compacted. There is a small workman\'s bench with household tools '
+                               'haphazardly hung on pegs or lying on the bench.', False)
 weapon1 = Weapons('candlestick', 'a heavy silver-plated candlestick with no candle.', 'Dining Room')
 weapon2 = Weapons('knife', "a sharp chef's knife", 'Kitchen')
 weapon3 = Weapons('poison', 'a vial of liquid with the poison warning on an otherwise unreadable label', 'Laboratory')
